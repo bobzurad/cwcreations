20e9b4943ea0456cd47aecdd8c5be481a397bc12
Bob Zurad <bob@zurad.net>
2014-12-18 20:27:50 -0600

    removed google and facebook authentication

--- app/_im/js/views/login.js
+++ app/_im/js/views/login.js
@@ -9,9 +9,8 @@ define(
       template: _.template(loginTemplate),
 
       events: {
-        "click #googleLogin": "authWithGoogle",
-        "click #facebookLogin": "authWithFacebook",
-        "click #passwordLogin": 'authWithPassword'
+        "click #passwordLogin": 'authWithPassword',
+        "keypress .form-control": 'authOnEnter'
       },
 
       render: function() {
@@ -27,45 +26,24 @@ define(
         this.$el.children().remove();
       },
 
-      authWithGoogle: function() {
-        this._login("google");
-      },
-
-      authWithFacebook: function() {
-        this._login("facebook");
+      authOnEnter: function(e) {
+        if (e.keyCode === 13) { //13 is enter key
+          this.authWithPassword();
+        }
       },
 
       authWithPassword: function() {
-        this._login('password');
-      },
-
-      _login: function(provider) {
-        var self = this;
-
-        if (provider === 'password') {
-          //email & password auth
-          this.model.firebase.authWithPassword({
-            email: this.$email.val(),
-            password: this.$password.val()
-          }, function(error, authData) {
-            self._loginCallback(error, authData);
-          })
-        } else {
-          //google & facebook auth
-          this.model.firebase.authWithOAuthPopup(provider, function(error, authData) {
-            self._loginCallback(error, authData);
-          });
-        }
-      },
-
-      _loginCallback: function(error, authData) {
-        if (error) {
-          $("#error").html(error.message);
-        } else {
-          window.location.hash = "#/list";  //we don't have access to router here, so can't call .navigate()
-        }
+        this.model.firebase.authWithPassword({
+          email: this.$email.val(),
+          password: this.$password.val()
+        }, function(error, authData) {
+          if (error) {
+            $("#error").html(error.message);
+          } else {
+            window.location.hash = "#/list";  //we don't have access to router here, so can't call .navigate()
+          }
+        });
       }
-
     });
 
     return LoginView;
